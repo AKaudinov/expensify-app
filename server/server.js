add8ad0df9eb7552d682cb8d361bb60d695c37d2
Amir Kaudinov <kaudinovamir@gmail.com>
2020-02-10 13:59:00 -0700

    -Adding express to the project -Adding server.js which uses express.js to run the built application via node -Adding start command to package.json so heroic can start up the express server -Adding heroku-postbuild command so it can actually execute the web pack configuration and serve the files via express server -Adding public files to ignore which can be built using web pack

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express(); //express application
+const publicPath = path.join(__dirname, '..', 'public'); //go up a directory, and then navigate to the public folder
+const port = process.evn.PORT || 3000; //get the port environment variable that heroku provides or just use 3000
+
+app.use(express.static(publicPath)); //something that runs for each request - serve up the files from publicPath
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+}); //run each time a get request is made to any route(and if there is no matching item in the public folder),
+//and return index.html that was built by react and redux
+//let react router take care of actual routing
+
+//HEROKU provides the port via environment variable
+app.listen(port, () =>{
+    console.log(`Server is up on port ${port}`);
+});
+
+
