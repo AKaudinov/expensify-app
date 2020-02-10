0954ef70b9204056bbe068c328c45d3f7629b28d
Amir Kaudinov <kaudinovamir@gmail.com>
2020-02-10 14:56:23 -0700

    -Adding numeral library so numbers/decimals, etc can be formatted on the ui -Refactoring the expenseListItem component to display and format the createdAt date and a proper amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,15 +4,17 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import {connect} from 'react-redux';
 import * as ExpenseActions from '../actions/expenses';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 //named export so it can be used for testing without being connected to redux store
 export const ExpenseListItem = ({expense, dispatch}) => { //destructure expense from props, it comes in as props.expense
     return (
       <div>
           <Link to={`edit/${expense.id}`}><h2>{expense.description}</h2></Link>
-          <h2>amount: {expense.amount}</h2>
-          <h3>createdAt: {expense.createdAt}</h3>
+          <h2>amount: {numeral(expense.amount / 100).format('$0,0.00')}</h2>
+          {/*convert expense.amount / 100 from cents to actual decimals*/}
+          <h3>createdAt: {moment(expense.createdAt).format('MM/DD/YYYY')}</h3>
       </div>
     );
 };
